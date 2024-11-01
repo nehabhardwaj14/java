8187d9ac43209b97e3b8f5aa8f7206c651c29f34
nehabhardwaj14 <149856647+nehabhardwaj14@users.noreply.github.com>
2024-11-01 22:21:34 +0530

    main

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPage() {
+    window.location.href = 'program.html'; // Replace with your target URL
+}
+function openPage1() {
+    window.location.href = 'https://nehabhardwaj14.github.io/php/'; // Replace with your target URL
+}
+function openPage2() {
+    window.location.href = 'program.html'; // Replace with your target URL
+}
